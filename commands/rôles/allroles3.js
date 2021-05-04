cd1c27351ae1cc7e7735f58dd8b93e79ab12c75b
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-04 16:48:42 +0200

    Update allroles3.js

--- commands/rôles/allroles3.js
+++ commands/rôles/allroles3.js
@@ -6,7 +6,7 @@ module.exports.run = (client,message,args) => {
   .setDescription("<a:alerte:835919931434467378> : News \n<a:giveaway2:835920882319884350> : Giveaways \n<a:love:835920118303031306> : Parteneriats")
   .setColor("#00BFFF")
 
-  client.channels.cache.get('835898363152039946').send(embed).then(message => {
+  client.channels.cache.get('839150422652223538').send(embed).then(message => {
     message.react('835919931434467378');
     message.react('835920882319884350');
     message.react('835920118303031306');
