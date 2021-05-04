8dec1e63d3c3bdda676b5e0037086cf73076f902
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-04 15:58:09 +0200

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,15 @@
+const { Client, Collection } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
+const randomstring = require("randomstring");
+const { loadCommands, loadEvents } = require("./util/loader");
+
+const client = new Client();
+require("./util/functions")(client);
+client.mongoose = require('./util/mongoose');
+client.commands = new Collection();
+
+loadCommands(client);
+loadEvents(client);
+client.mongoose.init();
+
+client.login(TOKEN);
