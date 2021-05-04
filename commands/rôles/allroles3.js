076e56ac6462399a66cdfcb63fadff3bde9f0eab
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-04 16:36:14 +0200

    Update allroles3.js

--- commands/rôles/allroles3.js
+++ commands/rôles/allroles3.js
@@ -2,8 +2,8 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports.run = (client,message,args) => {
   const embed = new MessageEmbed()
-  .setTitle("Vérification")
-  .setDescription("Cliquez sur la réaction ci-dessous pour accéder au reste du serveur")
+  .setTitle("Notifications")
+  .setDescription("<a:alerte:835919931434467378> \n<a:giveaway2:835920882319884350> \n<a:love:835920118303031306>")
   .setColor("#00BFFF")
 
   client.channels.cache.get('835898363152039946').send(embed).then(message => {
