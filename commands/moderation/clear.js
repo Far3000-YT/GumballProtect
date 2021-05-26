9e55914a5b9b3f7f40d4e085d690ad87913dab6c
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-26 09:36:02 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client,message,args) => {
-  if (isNaN(args[0]) || (args[0]<1 || args[0]>100)) return message.reply("Il faut spécifier un nombre entre 1 et 100 !");
+  if (isNaN(args[0]) || (args[0]<1 || args[0]>100)) return message.reply("It should be a number between 1 and 100 !");
 
   const messages = await message.channel.messages.fetch({
     limit: Math.min(args[0], 100),
@@ -13,17 +13,17 @@ module.exports.run = async (client,message,args) => {
 
   const embed = new MessageEmbed()
   .setTitle("Clear")
-  .setDescription(`__**Messages supprimés**__: ${args[0]}\n __**Salon**__: ${message.channel}`)
+  .setDescription(`__**Deleted messages**__: ${args[0]}\n __**Channel**__: ${message.channel}`)
   .setColor("#00BFFF")
 
   message.channel.send(embed);
-  client.channels.cache.get('835906671624716289').send(embed);
+  client.channels.cache.get('846831210289168384').send(embed);
 };
 
 module.exports.help = {
   name: 'clear',
   description: "Enlève un nombre spécifié de messages",
-  usage: '<nombre_de_messages>',
+  usage: '<number_of_messages>',
   isUserAdmin: false,
   permissions: true,
   args: true
