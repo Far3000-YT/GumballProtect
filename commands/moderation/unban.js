15e6b859f1da084657c6f7dead1402c363f8481a
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-26 09:40:11 +0200

    Update unban.js

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -4,16 +4,16 @@ module.exports.run = async (client,message,args) => {
   const user = await client.users.fetch(args[0]);
   const reason = (args.splice(1).join(' '));
 
-  if (!user) return message.reply("L\'utilisateur n'existe pas !")
+  if (!user) return message.reply("The user do not exist !")
   message.guild.members.unban(user)
 
   const embed = new MessageEmbed()
   .setTitle("Ban")
-  .setDescription(`L'utilisateur ${user.id} a été débanni par ${message.author.tag}`)
+  .setDescription(`${user.id} is now unbanned by ${message.author.tag}`)
   .setColor("#32CD32")
 
   message.channel.send(embed);
-  client.channels.cache.get('835906671624716289').send(embed);
+  client.channels.cache.get('846831210289168384').send(embed);
 };
 
 module.exports.help = {
