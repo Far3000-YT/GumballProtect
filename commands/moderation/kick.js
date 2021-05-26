508a2bb30b3017d5aa67a409fb031fd396ffb1fe
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-26 09:37:23 +0200

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -3,21 +3,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = (client,message,args) => {
   const user = message.mentions.users.first();
   const reason = (args.splice(1).join(' '));
-  user ? message.guild.member(user).kick(reason) : message.channel.send("L\'utilisateur n'existe pas !")
+  user ? message.guild.member(user).kick(reason) : message.channel.send("The user do not exist !")
 
   const embed = new MessageEmbed()
   .setTitle("Kick")
-  .setDescription(`**L'utilisateur ${user} a été kick par ${message.author.tag} !**\n __**Raison**__: ${reason}`)
+  .setDescription(`**${user} has been kicked by ${message.author.tag} !**\n __**Reason**__: ${reason}`)
   .setColor("#FFD700")
 
   message.channel.send(embed);
-  client.channels.cache.get('835906671624716289').send(embed);
+  client.channels.cache.get('846831210289168384').send(embed);
 };
 
 module.exports.help = {
   name: 'kick',
   description: "Kick un utilisateur",
-  usage: '<@utilisateur> <raison>',
+  usage: '<@user> <reason>',
   isUserAdmin: true,
   permissions: true,
   args: true
