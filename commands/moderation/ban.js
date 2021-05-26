339fa6499689fccb403b1b25bd9f2564e5af8475
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-05-26 09:34:45 +0200

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -3,15 +3,15 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = (client,message,args) => {
   const user = message.mentions.users.first();
   const reason = (args.splice(1).join(' '));
-  user ? message.guild.member(user).ban({ reason }) : message.channel.send("L\'utilisateur n'existe pas !")
+  user ? message.guild.member(user).ban({ reason }) : message.channel.send("The user do not exist !")
 
   const embed = new MessageEmbed()
   .setTitle("Ban")
-  .setDescription(`L'utilisateur ${user} a été banni par ${message.author.tag}!**\n __**Raison**__: ${reason}`)
+  .setDescription(`**${user} has been banned by ${message.author.tag}!**\n __**Reason**__: ${reason}`)
   .setColor("#dc143c")
 
   message.channel.send(embed);
-  client.channels.cache.get('835906671624716289').send(embed);
+  client.channels.cache.get('846831210289168384').send(embed);
 };
 
 module.exports.help = {
