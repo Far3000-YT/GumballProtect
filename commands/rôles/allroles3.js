e58a036341a11ed8ead75d9d69f0185e821fceb3
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-06-15 12:28:36 +0200

    Update allroles3.js

--- commands/rôles/allroles3.js
+++ commands/rôles/allroles3.js
@@ -3,14 +3,14 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = (client,message,args) => {
   
   const embed = new MessageEmbed()
-  .setTitle("Notification Roles")
-  .setDescription("<a:news:846845623784570970> : News role \n<a:giveaway:846845623662542848> : Giveaways role \n<a:partner:846848375281418311> : Partnerships notification role")
+  .setTitle("Notifications")
+  .setDescription("<a:pin:835920290658779146> : Notifications news \n<a:giveaway2:835920882319884350> : Notifications giveaways \n<a:love:835920118303031306> : Notifications parteneriats")
   .setColor("#00BFFF")
 
-  client.channels.cache.get('846840240135733299').send(embed).then(message => {
-    message.react('846845623784570970');
-    message.react('846845623662542848');
-    message.react('846848375281418311');
+  client.channels.cache.get('854260243792658462').send(embed).then(message => {
+    message.react('835920290658779146');
+    message.react('835920882319884350');
+    message.react('835920118303031306');
   })
 }
 
