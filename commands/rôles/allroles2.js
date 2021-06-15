b932f89390232a0147717faf2e831a37fef7a87d
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-06-15 12:24:19 +0200

    Update allroles2.js

--- commands/rôles/allroles2.js
+++ commands/rôles/allroles2.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = (client,message,args) => {
-  const rougeRole = message.guild.roles.cache.get('835933265328078899');
-  const orangeRole = message.guild.roles.cache.get('835933437918576750');
-  const jauneRole = message.guild.roles.cache.get('835933404360867922');
-  const vertRole = message.guild.roles.cache.get('835933371552366632');
-  const bleuRole = message.guild.roles.cache.get('835933337046220880');
-  const violetRole = message.guild.roles.cache.get('835933511872675850');
+  const rougeRole = message.guild.roles.cache.get('854260214550495232');
+  const orangeRole = message.guild.roles.cache.get('854260215292362802');
+  const jauneRole = message.guild.roles.cache.get('854260216043405332');
+  const vertRole = message.guild.roles.cache.get('854260216723537931');
+  const bleuRole = message.guild.roles.cache.get('854260217454133289');
+  const violetRole = message.guild.roles.cache.get('854260218333757451');
 
   const embed = new MessageEmbed()
   .setTitle("Couleurs")
   .setDescription("Cliquez sur les réactions ci-dessous pour choisir vos couleurs")
   .setColor("#00BFFF")
 
-  client.channels.cache.get('835898278876938270').send(embed).then(message => {
+  client.channels.cache.get('854260242601476156').send(embed).then(message => {
     message.react('836980372209926144');
     message.react('836980373455765534');
     message.react('836980371983302726');
