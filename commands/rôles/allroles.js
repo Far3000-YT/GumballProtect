7388d55b00dc469cf2aa8cc7a24430ff34a5eb05
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2021-06-15 12:20:55 +0200

    Update allroles.js

--- commands/rôles/allroles.js
+++ commands/rôles/allroles.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = (client,message,args) => {
-  const valideRole = message.guild.roles.cache.get('835898952372191242');
+  const valideRole = message.guild.roles.cache.get('854260219935588352');
 
   const embed = new MessageEmbed()
   .setTitle("Vérification")
   .setDescription("Cliquez sur la réaction ci-dessous pour accéder au reste du serveur")
   .setColor("#00BFFF")
 
-  client.channels.cache.get('835897653111685160').send(embed).then(message => {
+  client.channels.cache.get('854260236008685588').send(embed).then(message => {
     message.react('835920851299729438');
   })
 }
