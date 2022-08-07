592ba212846349b1cf64de927cd63025ea7d96de
Far3000-YT <83653727+Far3000-YT@users.noreply.github.com>
2022-08-07 02:19:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    TOKEN: "ODU5MDcwNTc2MTQzMTA2MDQ4.YNnV0g.jUi9eeP3DgnqKLPlXtb2EkksW94",
+    TOKEN: "ODU5MDcwN*****"""""*DQ4.YNnV0g.jUi9eeP3DgnqKLPlXtb2EkksW94",
     PREFIX: "=",
-    DBCONNECTION: "mongodb+srv://Far3000:Gunfive2030@gumballrewards.6kbpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    DBCONNECTION: "mongodb+srv://Far3000:******@gumballrewards.6kbpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     DEFAULTSETTINGS: {
         prefix: "=",
         logChannel: "logs",
